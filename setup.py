4399a56652aeeb0ac62a7f6ac9443d16a6605422
Vaibhav Singh <singh.vaibhav.83@gmail.com>
2015-11-27 10:18:24 -0800

    minimal changes to align with Jarvis

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
-setup(name='JARVIS',
+setup(
+      name='JARVIS',
       version='1.0',
-      package_dir={'loadData': 'UTILS'},
-      packages=['loadData'],
+      package_dir={'loadData': 'UTILS',
+                   'convNet_tensorFlow': 'CONVNET',
+                   'convNet_theano': 'CONVNET'
+                    },
+      packages=['loadData','convNet_tensorFlow','convNet_theano'  ]
       )
